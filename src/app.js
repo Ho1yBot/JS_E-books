0ba777641fccc7e90a21e463e9b728d423b8a17a
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-06-17 15:22:58 +0300

    Конвертёр валют. Intl API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 'use strict';
 
-// Написать функцию, которая принимает min и max 
-// и возвращает случайное целое число между ними, вкл их
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max-min+1) + min);
+function convertCurrency(sum, from, to) {
+  const allCurrency = [
+    { name: "USD", mult: 1 },
+    { name: "RUB", mult: 1 / 60 },
+    { name: "EUR", mult: 1.1},
+  ]
+  const fromExist = allCurrency.find(c => c.name === from);
+  if (!fromExist){
+    return null
+  }
+  const toExist = allCurrency.find(c => c.name === to);
+  if (!toExist){
+    return null
+  }
+  
+  return new Intl.NumberFormat("ru-RU", {style: "currency", currency: to}).format(sum * fromExist.mult / toExist.mult)
 }
 
-console.log(random(3, 10));
+console.log(convertCurrency(100, "RUB", "USD")); 
