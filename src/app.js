112f518bdd50115dabe7413c8d313c8dedac684d
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-07-14 23:37:23 +0500

    запрос на сервер и расчёт

--- src/app.js
+++ src/app.js
@@ -1,52 +1,19 @@
 "use strict"
 
-class Enemy {
-  constructor(health) {
-    this.health = health
-  };
+const request = new XMLHttpRequest();
+let number = 1;
+let id = 1
 
-  recieveDamage(damage){
-    if (this.hitPoint <= 0) {
-      return console.log("Enemy is killed");
-    }
-    this.health = this.health - damage
-  }
+request.open("GET", "https://dummyjson.com/products/" );
+request.send();
 
-}
+request.addEventListener("load", function () {
+  const data = JSON.parse(this.responseText)
+  const sum = data.products.reduce((acc, product) => {
+    acc = acc + product.price
+    return acc
+  }, 0)
+  console.log(sum/data.products.length);
+})
 
-class Sword{
-  #damage
-  constructor(damage){
-    this.#damage = damage
-  }
 
-  strike(enemy) {
-    enemy.recieveDamage(this.#damage)
-  }
-
-}
-
-class Orc extends Enemy {
-  constructor(health) {
-    super(health)
-  };
-
-  recieveDamage(damage){
-    if(Math.random()*100 < 50){
-      return console.log("miss");
-    } else {
-      this.health = this.health - damage
-    }
-    if (this.health <= 0) {
-      this.health = 0
-      return console.log("Enemy is killed");
-    }
-  }
-}
-
-const orkLoh = new Orc (100);
-const sword = new Sword (40)
-console.log(orkLoh);
-sword.strike(orkLoh)
-sword.strike(orkLoh)
-console.log(orkLoh);
