f0a9a25cf6268b9d039c001ba778455cdf627e54
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-08-01 23:32:01 +0500

    Получение местоположения пользователя. geolocation API, fetch

--- src/app.js
+++ src/app.js
@@ -1,27 +1,46 @@
-"use strict"
-function getDataOrErrorText(url, errorText) {
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error(errorText);
+"use strict";
+
+function getMyCoordinates() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+      },
+      (error) => {
+        reject(error);
       }
-    })
+    );
+  });
 }
 
-getDataOrErrorText("https://dummyjson.com/products", "Can not get products")
-  .then(({ products }) => {
-    return getDataOrErrorText("https://dummyjson.com/products/" + products[0].id, "Can not get products")
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    const el = document.querySelector(".filter")
-    el.innerHTML = error.message
-  })
+async function getMyPlace() {
+  try {
+    const { latitude, longitude } = await getMyCoordinates();
+    const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    console.log(data.city);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
+getMyPlace();
 
+// function getMyCoordinates() {
 
+//   navigator.geolocation.getCurrentPosition(
+//     (position) => {
 
+//       console.log(position)
+//     }
+//   );
+
+// }
+
+// getMyCoordinates()
