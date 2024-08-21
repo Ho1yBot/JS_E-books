e7e1d9236db3937a5012df315427b981ca736059
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-08-22 02:19:05 +0500

    Updates

--- dist/app.js
+++ dist/app.js
@@ -1126,15 +1126,15 @@
             this.el.classList.add("header");
             this.el.innerHTML = `
         <div>
-            <img src="../../static/logo.svg" alt="Логотип" />
+            <img src="./static/logo.svg" alt="Логотип" />
         </div>
         <div class="menu">
             <a class="menu__item" href="#">
-                <img src="../../static/search.svg" alt="Поиск иконка" />
+                <img src="./static/search.svg" alt="Поиск иконка" />
                 Поиск книг
             </a>
             <a class="menu__item" href="#favorites">
-                <img src="../../static/favorites.svg" alt="Избранное" />
+                <img src="./static/favorites.svg" alt="Избранное" />
                 Избранное
                 <div class="menu__counter">
                     ${this.appState.favorites.length}
@@ -1170,7 +1170,7 @@
             <img src="./static/search.svg" alt="Иконка поиска"/>
         </div>
         <button aria-label="Искать">
-            <img src="../../static/search-white.svg" alt="Иконка поиска"/>
+            <img src="./static/search-white.svg" alt="Иконка поиска"/>
         </button>
         `;
             this.el.querySelector("button").addEventListener("click", this.search.bind(this));
@@ -1220,8 +1220,8 @@
                 <div class="card__footer">
                     <button class="button__add ${existInFavorites ? "button__active" : ""}">
                          ${existInFavorites
-                        ? "<img src='../../static/favorites.svg' />"
-                        : "<img src='../../static/favorites-white.svg' />"}
+                        ? "<img src='./static/favorites.svg' />"
+                        : "<img src='./static/favorites-white.svg' />"}
                     </button>
                  </div>
             </div>
