6bd9b27f77d0244e8ee63aed4ee40ea054b389b5
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-08-22 02:13:10 +0500

    Реализована логика добавления/удаления в избранное

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,10 +8,18 @@ export class Card extends DivComponent {
         this.cardState = cardState;
     }
 
+    #addToFavorites() {
+        this.appState.favorites.push(this.cardState)
+    }
+
+    #deleteFromFavorites() {
+        this.appState.favorites = this.appState.favorites.filter(book => book.key !== this.cardState.key)
+    }
+
     render() {
         this.el.classList.add("card");
-        const existFavorites = this.appState.favorites.find(
-            b => b.key == this.cardState.key
+        const existInFavorites = this.appState.favorites.find(
+            book => book.key == this.cardState.key
         )
         this.el.innerHTML = `
             <div class="card__image"> 
@@ -25,18 +33,22 @@ export class Card extends DivComponent {
                     ${this.cardState.title}
                 </div>
                 <div class="card__author">
-                    ${this.cardState.author_name ? this.cardState.author_name[0] : "Незадано"}
+                    ${this.cardState.author_name ? this.cardState.author_name[0] : "Не задано"}
                  </div>
                 <div class="card__footer">
-                    <button class="button_add ${existFavorites ? "buttom__active" : ""}">
-                         ${existFavorites 
-                            ? "<img src='../../static/favorites.svg' />" 
-                            : "<img src='../../static/favorites-white.svg' />" }
+                    <button class="button__add ${existInFavorites ? "button__active" : ""}">
+                         ${existInFavorites
+                        ? "<img src='../../static/favorites.svg' />"
+                        : "<img src='../../static/favorites-white.svg' />"}
                     </button>
                  </div>
-
             </div>
         `
+        if (existInFavorites) {
+            this.el.querySelector('button').addEventListener("click", this.#deleteFromFavorites.bind(this))
+        } else {
+            this.el.querySelector('button').addEventListener("click", this.#addToFavorites.bind(this))
+        }
         return this.el
     }
 }
